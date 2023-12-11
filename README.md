35f715f95d84d2253f760bb0ae2880322f7a2c49
duclm278 <palered548@gmail.com>
2023-12-11 21:18:10 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Hack QR
+
+- Run module api
+  
+```shell
+cd api
+pip install -r requirements.txt
+python3 server.py
+```
+
+- Run module web
+
+```shell
+cd web
+python3 -m http.server 5500
+```
+
+- Open website: <http://localhost:5500/>
---
+++ api/generate_broken_qr.py
@@ -0,0 +1,27 @@
+import qr
+
+
+def generate_broken_qr(message, ecc, version, mask, filename):
+    q0 = qr.generate_qr_code(message, ecc, version, mask)
+    qr_matrix = qr.qr_matrix(q0)
+    broken_qr_matrix = cover_format_modules(qr_matrix)
+    output_path = "./demo/" + filename + ".png"
+    qr.qr_matrix_image(broken_qr_matrix, output_path, show=False)
+    return output_path
+
+
+def cover_format_modules(qr_matrix):
+    """
+    Takes in numpy matrix of qr_matrix and changes any 0s in the format
+    specification areas to 1s
+    """
+    for i in range(8):
+        qr_matrix[8][i] = 1  # horizontal top left
+        qr_matrix[8][-i] = 1  # horizontal top right
+        qr_matrix[i][8] = 1  # vertical top left
+        qr_matrix[-i][8] = 1  # vertical bottom left
+    return qr_matrix
+
+
+# if __name__ == '__main__':
+#     generate_broken_qr('./tests/target/yahoo.png')
